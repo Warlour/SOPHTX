e32840f7708be84345686d872dd8f58f626504fb
MathiasKES <math224p@zbc.dk>
2021-12-07 19:18:25 +0100

    Followed another guide

--- AI.py
+++ AI.py
@@ -1,33 +1,20 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import tensorflow as tf
+from tensorflow import keras
+from matplotlib import pyplot as plt
+import numpy as np
 
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
-print(f"Tensorflow version: {tf.__version__}")
+len(x_train)
+# 60000
 
-learn = tf.contrib.learn
+len(x_test)
+# 10000
 
-tf.logging.set_verbosity(tf.logging.ERROR)
+# Finding the shape of individual sample
+x_train[0].shape
+# (28, 28)
 
-# Importing dataset using MNIST Data
-mnist = learn.datasets.load_dataset('mnist')
-data = mnist.train.images
-labels = np.asarray(mnist.train.labels, dtype = np.int32)
-test_data = mnist.test.images
-test_labels = np.asarray(mnist.test.labels, dtype = np.int32)
-# Outputs the retrieval of the dataset
+x_train[0]
 
-# Making the dataset
-max_examples = 10000
-data = data[:max_examples]
-labels = labels[:max_examples]
-
-# Plotting dataset via matplotlib
-def display(i):
-    img = test_data[i]
-    plt.title('label : {}'.format(test_labels[i]))
-    plt.imshow(img.reshape((28, 28)))
-
-# Display image as 28 by 28 pixels
-display(0)
+plt.matshow(x_train[0])
