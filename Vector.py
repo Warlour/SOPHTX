989244167b7bf64398078936d4084d97bf599680
MathiasKES <math224p@zbc.dk>
2021-12-07 18:34:07 +0100

    Initial commit of Vector-Class

--- Vector.py
+++ Vector.py
@@ -1,2 +1,29 @@
 class Vector:
-    pass
+    def __init__(self, a = []):
+        self.size = len(a)
+        self.data = a
+
+    def __add__(self, other):
+        if (self.size != other.size):
+            raise ValueError("Vectors are not of same length")
+
+        sum = []
+
+        for _ in self.data:
+            sum.append(0)
+
+        for i in self.data:
+            print()
+
+        #return Vector(sum)
+
+    def __str__(self):
+        return str(self.data)
+
+if (__name__ == "__main__"):
+    data2 = [5, 2, 2]
+    b = Vector(data2)
+
+    print(b)
+    print(b.size)
+    print(b+b)
