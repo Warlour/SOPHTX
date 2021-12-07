10b20a29adbe73eda07aa81fc03d1d1113eec48f
MathiasKES <math224p@zbc.dk>
2021-12-07 18:30:06 +0100

    Commented out mvP function

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from Matrix import Matrix
 from Vector import Vector
 
-def mvP(matrix, vector):
-    '''Returns a vector\n\nParameters: matrix, vector'''
+# def mvP(matrix, vector):
+#     '''Returns a vector\n\nParameters: matrix, vector'''
 
-    #if(type(matrix) != type(Matrix())):
-        #raise TypeError("First argument should be of type Matrix")
-    #if(type(vector) != type(Vector())):
-        #raise TypeError("Second argument should be of type Vector")
-    row = len(matrix)
-    col = len(matrix[0])
-    mvP = []
+#     #if(type(matrix) != type(Matrix())):
+#         #raise TypeError("First argument should be of type Matrix")
+#     #if(type(vector) != type(Vector())):
+#         #raise TypeError("Second argument should be of type Vector")
+#     row = len(matrix)
+#     col = len(matrix[0])
+#     mvP = []
 
-    for _ in range(row):
-        mvP.append(0)
+#     for _ in range(row):
+#         mvP.append(0)
 
-    for i in range(row):
-        for j in range(col):
-            mvP[i] = (mvP[i] + matrix[i][j] * vector[j])
-    return mvP
+#     for i in range(row):
+#         for j in range(col):
+#             mvP[i] = (mvP[i] + matrix[i][j] * vector[j])
+#     return mvP
 
-print(mvP([[1,2,3],[4,5,6]], [1,2,3]))
+data1 = [[1, 2], [6, 1], [8, 2], [9, 3]]
+a = Matrix(data1)
+
+# Size of 'a' matrix: [row, column]
+print(f"Size of a: {a.size}")
