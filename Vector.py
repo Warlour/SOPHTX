ee0440c8eb194ae65c94142cf9edbbcefee5d8a7
MathiasKES <math224p@zbc.dk>
2021-12-08 13:49:58 +0100

    Added addition and "unability" to multiply

--- Vector.py
+++ Vector.py
@@ -8,14 +8,19 @@ class Vector:
             raise ValueError("Vectors are not of same length")
 
         sum = []
-
         for _ in self.data:
             sum.append(0)
 
-        for i in self.data:
-            print()
+        for i in range(len(sum)):
+            sum[i] = self.data[i] + other.data[i]
 
-        #return Vector(sum)
+        return Vector(sum)
+
+    def __mul__(self, other):
+        return "You cannot multiply two vectors. However, you can calculate the scalarproduct using scalar(vector * vector)"
+    
+    def scalar(self, other):
+        pass
 
     def __str__(self):
         return str(self.data)
@@ -24,6 +29,6 @@ if (__name__ == "__main__"):
     data2 = [5, 2, 2]
     b = Vector(data2)
 
-    print(b)
-    print(b.size)
-    print(b+b)
+    #print(b)
+    #print(b.size)
+    print(b*b)
