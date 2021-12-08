58c340cddd2f4fd3db04014d30e288a827343da2
MathiasKES <math224p@zbc.dk>
2021-12-08 19:49:40 +0100

    Indsatte i en try for at kun skrive fejlkoden, og ikke steder i koden

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from Matrix import Matrix
 from Vector import Vector
 import ext_class_func as ecf
 
-data1 = [[1, 2], [6, 1], [8, 2], [9, 3]]
-a = Matrix(data1)
+try:
+    data1 = [[1, 2], [6, 1], [8, 2], [9, 3]]
+    a = Matrix(data1)
 
-data2 = [3, 4]
-b = Vector(data2)
+    data2 = [[3, 4], [5, 8]]
+    b = Matrix(data2)
 
-print(ecf.mvP(a, b))
+    print(a + b)
+except Exception as e:
+    print(e)
